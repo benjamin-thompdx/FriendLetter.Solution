bd1076be6672ed11732058d452e27c6de61a57f2
Benjamin Thom <jaminthom@gmail.com>
2020-03-08 14:56:47 -0700

    add comments to Startup.cs to demonstrate what each piece of code is doing

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ namespace FriendLetter
 {
   public class Startup
   {
+    //This constructor will create an iteration of the Startup class that contains specific settings and variables to run our project successfully. It's required for configuring a basic ASP.NET Core MVC project.
 
     public Startup(IHostingEnvironment env)
     {
@@ -17,21 +18,26 @@ namespace FriendLetter
       Configuration = builder.Build();
     }
 
+    //This line is the process of adding custom configurations to our project.
     public IConfigurationRoot Configuration { get; }
 
+    //ConfigureServices() is a required built-in method used to set up an application's server. We can use it to configure other framework services.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
     }
 
+    //The Configure() method is built-in and required in all ASP.NET Core apps. ASP.NET calls Configure() when the app launches. It's responsible for telling our app how to handle requests to the server.
     public void Configure(IApplicationBuilder app)
     {
+      //app.UseMvc(...), tells our app to use the MVC framework to respond to HTTP requests. This block of code also sets up default routing for our application. It tells the project to use the Index action of the Home Controller as the default route. This will be our homepage. 
       app.UseMvc(routes =>
       {
           routes.MapRoute(
               name: "default",
               template: "{controller=Home}/{action=Index}/{id?}");
       });
+      //app.Run(...), is not actually required to successfully launch our project. However, it will allow us to test that our Configure() method is working properly.
       app.Run(async (context) =>
       {
           await context.Response.WriteAsync("Hello World!");
